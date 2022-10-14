5b2fae266533505be618a72cf10cb605d3f37e9c
heshan97 <heshanw2018@gmail.com>
2022-10-14 16:14:12 +0530

    login update

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -8,40 +8,40 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 
 const Login = () => {
-  return (
-    <div className="main">
-    <div className="sub-main">
-      <div>
-      <div className='imgs'>
-        <div className='container-image'>
-          <FontAwesomeIcon icon={faUser}/>
-        </div>
+	return (
+		<div className="main">
+			<div className="sub-main">
+				<div>
+					<div className='imgs'>
+						<div className='container-image'>
+							<FontAwesomeIcon icon={faUser} size="3x" />
+						</div>
 
-      </div>
-      
-      <div>
-        <h1>Login Page</h1>
-        <div>
-          <img src={email} alt="email" className='email'/>
-          <input type= "text" placeholder = "user name" className='name'/>
-        </div>
-        
-        <div className='second-input'>
-          <img src={pass} alt="pass" className='pass'/>
-          <input type= "text" placeholder = "password" className='name'/>
-        </div>
-        <div className='login-button'>
-        <button>Login</button>
-        </div>
-      </div>
-      
-        <p  className='link'>
-          <a href='#'>Forgot password</a> or <a href='#'>Sign Up</a>
-        </p>
-        </div>
-    </div>
-    </div>
-  )
+					</div>
+
+					<div>
+						<h1>Login Page</h1>
+						<div>
+							<img src={email} alt="email" className='email' />
+							<input type="text" placeholder="user name" className='name' />
+						</div>
+
+						<div className='second-input'>
+							<img src={pass} alt="pass" className='pass' />
+							<input type="text" placeholder="password" className='name' />
+						</div>
+						<div className='login-button'>
+							<button>Login</button>
+						</div>
+					</div>
+
+					<p className='link'>
+						<a href='#'>Forgot password</a> or <a href='#'>Sign Up</a>
+					</p>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Login
