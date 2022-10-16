1e32af08ce8616d7541b22b5629ed40295722638
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2022-10-16 15:04:56 +0530

    feat: enrolled courses

---
+++ src/pages/MyCourses.tsx
@@ -0,0 +1,51 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useEffect } from "react";
+import { Navigate } from "react-router-dom";
+
+import CourseCard from "../components/CourseCard";
+import { useUser } from "../context/UserProvider";
+import useFetch from "../hooks/useFectch";
+
+const MyCourses = () => {
+  const { data, fetchData, isFetching, isError } = useFetch();
+  const { user } = useUser();
+
+  useEffect(() => {
+    const fetch = async () => {
+      await fetchData(`/student/enrolledCourses/${user.studentID}`, {
+        type: "GET",
+      });
+    };
+    fetch();
+  }, []);
+
+ 
+
+  return (
+    <Box display={"flex"} justifyContent="center">
+      <Box p={"3rem"} boxSizing={"border-box"} height="100vh">
+        {data &&
+          !isError &&
+          !isFetching &&
+          data.map((course: any, id: number) => {
+            return (
+              <CourseCard
+                id={course.courseID}
+                title={course.courseName}
+                description={course.description}
+                key={id}
+                type={"Enrolled"}
+              />
+            );
+          })}
+
+        {data && !isError && !isFetching && data.length === 0 && (
+          <Typography fontSize={"2rem"}>You Haven't enrolled To Any Course!</Typography>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default MyCourses;
