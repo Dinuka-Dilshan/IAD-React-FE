d36e26341e7248889a179c00f2829bf3233e792e
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2022-10-16 15:02:01 +0530

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,52 @@
-import { Box, Typography } from "@mui/material";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+import Announcements from "./pages/Announcements";
+import AvailableCourses from "./pages/AvailableCourses";
+import Course from "./pages/Course";
+import Login from "./pages/Login";
+import MainLayout from "./pages/MainLayout";
+import MyCourses from "./pages/MyCourses";
+import Profile from "./pages/Profile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "mycourses",
+        element: <MyCourses />,
+      },
+      {
+        path: "availablecourses",
+        element: <AvailableCourses />,
+      },
+      {
+        path: "announcements",
+        element: <Announcements />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+      {
+        path: "mycourses/:id",
+        element: <Course />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <Box
-      height={"100vh"}
-      width={"100vw"}
-      display={"flex"}
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Typography fontSize={'2rem'}>Hi!</Typography>
-    </Box>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
